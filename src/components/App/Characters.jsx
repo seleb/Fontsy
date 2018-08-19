cf6fd02ed856cabd492aaea93b160a575e4f2026
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2018-08-19 03:13:09 -0400

    add basic paging
    need to page characters, otherwise large font sets hang

--- src/components/App/Characters.jsx
+++ src/components/App/Characters.jsx
@@ -7,19 +7,60 @@ import Character from './Characters/Character';
 
 import './Characters.css';
 
-export function Characters({
-	characters = {},
-}) {
-	return (
-		<div className="characters">
-			{Object.keys(characters).map(character => <Character character={character} />)}
-		</div>
-	);
+const charactersPerRow = 16;
+const rows = 4;
+
+export class Characters extends Component {
+	constructor() {
+		super();
+		this.state = {
+			row: 0,
+		};
+	}
+
+	onWheel = event => {
+		this.setState(({ row }) => ({
+			row: Math.max(0, row + Math.sign(event.deltaY)),
+		}));
+		event.preventDefault();
+	}
+
+	componentDidUpdate() {
+		const {
+			row = 0,
+		} = this.state;
+		const {
+			characters = [],
+		} = this.props;
+		const limit = Math.max(0, Math.floor(characters.length / charactersPerRow) - 4);
+		if (row > limit) {
+			this.setState({
+				row: limit,
+			});
+		}
+	}
+
+	render({
+		characters = [],
+	}, {
+		row = 0,
+	}) {
+		const style = {
+			gridTemplateColumns: `repeat(${charactersPerRow}, 1fr)`,
+		};
+		return (
+			<div className="characters" style={style} onWheel={this.onWheel}>
+				{characters
+					.slice(row * charactersPerRow, (row + rows) * charactersPerRow)
+					.map(character => <Character character={character} />)}
+			</div>
+		);
+	}
 }
 
 export function mapStateToProps(state) {
 	return {
-		characters: getCharacters(state),
+		characters: Object.keys(getCharacters(state)),
 	};
 }
 
