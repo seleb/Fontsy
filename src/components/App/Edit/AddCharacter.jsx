c98da46cc7843f586953ec520feeec6b2a39c329
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2018-08-21 01:39:16 -0400

    synced character and code point input

--- src/components/App/Edit/AddCharacter.jsx
+++ src/components/App/Edit/AddCharacter.jsx
@@ -42,13 +42,24 @@ export class AddCharacter extends Component {
 		}
 	}
 
-	onChange = ({
+	onChangeNumber = ({
 		currentTarget: {
-			value = '',
+			value,
 		} = {},
 	}) => {
 		this.setState({
-			character: parseInt(event.currentTarget.value, 10),
+			character: parseInt((event.currentTarget.value || '0'), 10),
+		});
+		event.preventDefault();
+	}
+
+	onChangeString = ({
+		currentTarget: {
+			value,
+		} = {},
+	}) => {
+		this.setState({
+			character: parseInt((event.currentTarget.value || '\0').charCodeAt(0), 10),
 		});
 		event.preventDefault();
 	}
@@ -61,9 +72,11 @@ export class AddCharacter extends Component {
 		const hasCharacter = characters.includes(character.toString(10));
 		return (
 			<main className="add-character">
-				<label for="add-character-input">Unicode Code Point: </label>
-				<input type="number" name="add-character-input" min={0} value={character} onChange={this.onChange} />
+				<label for="add-character-input-number">Unicode Code Point: </label>
+				<input type="number" name="add-character-input-number" min={0} max={0x10FFFF} value={character} onChange={this.onChangeNumber} />
 				<a href="https://en.wikipedia.org/wiki/List_of_Unicode_characters" target="_blank" rel="noopener noreferrer">❓</a>
+				<label for="add-character-input-string">Unicode Character: </label>
+				<input type="text" name="add-character-input-string" maxLength={1} value={String.fromCodePoint(character)} onChange={this.onChangeString} />
 				<Toggle
 					title={`${hasCharacter ? 'Remove' : 'Add'} character at code point ${character}`}
 					onClick={hasCharacter ? this.remove : this.add}
