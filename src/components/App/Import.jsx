2c1f88bab83c65e6bdf56cc7388aeb46d26dd9f9
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2018-08-21 01:13:00 -0400

    add option for import mode

--- src/components/App/Import.jsx
+++ src/components/App/Import.jsx
@@ -4,11 +4,21 @@ import { connect } from 'preact-redux';
 import { imageToFont } from '../../lib/imageConverter';
 import { textToFont } from '../../lib/fontConverter';
 
-import { setFont } from '../../reducers/font';
+import {
+	setFont,
+	mergeFont,
+} from '../../reducers/font';
 
 import './Import.css';
 
 export class Import extends Component {
+	constructor() {
+		super();
+		this.state = {
+			merge: false,
+		};
+	}
+
 	onChange = ({
 		currentTarget: {
 			files: {
@@ -38,23 +48,52 @@ export class Import extends Component {
 			} else {
 				throw new Error('Unsupported file type');
 			}
-			getFont().then(this.props.setFont);
+			getFont().then(this.state.merge ? this.props.mergeFont : this.props.setFont);
 		};
 
 		reader.readAsDataURL(file);
+	}
+
+	setModeMerge = () => {
+		this.setState({
+			merge: true,
+		})
+	}
+
+	setModeReplace = () => {
+		this.setState({
+			merge: false,
+		})
 	}
 
 	render({
 		src = '',
 		width = 0,
 		height = 0,
+	}, {
+		merge = false,
 	}) {
-		return <input type="file" name="file" accept=".png, .bitsyfont" onChange={this.onChange} />
+		return (
+			<div className="import">
+				<label for="file">File: </label>
+				<input className="file-input" type="file" name="file" accept=".png, .bitsyfont" onChange={this.onChange} />
+				<label for="mode">Mode: </label>
+				<label for="merge" onClick={this.setModeMerge} title="Overwrites existing characters in both fonts; leaves characters only in first font intact">
+					Merge
+					<input type="radio" radioGroup="mode" name="merge" value="merge" checked={merge} />
+				</label>
+				<label for="replace" onClick={this.setModeReplace} title="Removes all existing characters">
+					Replace
+					<input type="radio" radioGroup="mode" name="replace" value="replace" checked={!merge} />
+				</label>
+			</div>
+		);
 	}
 }
 
 export const mapDispatchToProps = {
 	setFont,
+	mergeFont,
 }
 
 export default connect(undefined, mapDispatchToProps)(Import);
