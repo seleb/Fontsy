d1ab4eed4ae56f9edf022d8f5d97770d9caf9046
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2021-05-15 20:19:17 -0400

    refactor: move google font to index

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,15 +8,6 @@ import Export from './App/Export';
 import Collapsible from './Collapsible';
 
 export default class App {
-	componentDidMount() {
-		// add google font
-		// probably a better way to do this but this works for now
-		const link = document.createElement('link');
-		link.href = 'https://fonts.googleapis.com/css?family=Noto+Sans&amp;subset=cyrillic,cyrillic-ext,devanagari,greek,greek-ext,latin-ext,vietnamese';
-		link.rel = 'stylesheet';
-		document.head.appendChild(link);
-	}
-
 	render() {
 		return (
 			<div class="app">
