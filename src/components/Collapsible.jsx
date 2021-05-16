c0af812a5ac9e468f9e3cc2f2808430b9015e9b7
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2021-05-15 23:48:04 -0400

    fix: replace collapsible with semantic html

--- src/components/Collapsible.jsx
+++ src/components/Collapsible.jsx
@@ -1,22 +1,15 @@
 /* @jsx h */
 import { h } from 'preact';
-import { useCallback, useState } from 'preact/hooks';
 import './Collapsible.css';
-import Toggle from './Toggle';
 
 export default function Collapsible({ children, header = '', description = '', open: startOpen = false }) {
-	const [open, setOpen] = useState(startOpen);
-	const toggleOpen = useCallback(() => {
-		setOpen(!open);
-	}, [open]);
 	return (
-		<section className={`collapsible ${open ? 'open' : 'closed'}`}>
-			<header className="header">
-				<Toggle onClick={toggleOpen} enabled={open} />
-				<h1 className="name">{header}</h1>
-				<h2 className="description">{description}</h2>
-			</header>
-			{open && <main className="content">{children}</main>}
-		</section>
+		<details open={startOpen} className="collapsible">
+			<summary>
+				<h1>{header}</h1>
+				<h2>{description}</h2>
+			</summary>
+			<section>{children}</section>
+		</details>
 	);
 }
